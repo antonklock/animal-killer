7a196257a06fa1e1d74ecca5d92a69805e493c0c
Anton Klock <klock.anton@gmail.com>
2022-10-07 15:58:06 +0200

    added rive and gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,142 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import cow from "./assets/cow.json";
+//@ts-ignore
+import chickenTest from "./assets/chicken_test.riv";
+import {
+  Alignment,
+  Fit,
+  Layout,
+  useRive,
+  useStateMachineInput,
+} from "@rive-app/react-canvas";
+import gsap from "gsap";
 
 function App() {
+  const industryRef = useRef(null);
+  const [playerCount, setPlayerCount] = useState(0);
+  const [industryCount, setIndustryCount] = useState(0);
+  const [playerAnim, setPlayerAnim] = useState(cow);
+  const [explodeVisibility, setExplodeVisibility] = useState<
+    "hidden" | "visible"
+  >("hidden");
+
+  let animSize = "200px";
+  let rate = 0.05;
+
+  const explodeStyle: React.CSSProperties = {
+    width: "150px",
+    position: "absolute",
+    top: "310px",
+    left: "auto",
+    visibility: explodeVisibility,
+  };
+
+  const handleIncreasePlayerCount = () => {
+    setPlayerCount(playerCount + 1);
+    // @ts-ignore
+    // setPlayerAnim(explode);
+    explodePlayer?.fire();
+    animSize = "10px";
+    setExplodeVisibility("visible");
+  };
+
+  useEffect(() => {
+    const addIndustryPoint = setInterval(() => {
+      setIndustryCount((count) => count + 1);
+    }, rate * 1000);
+    return () => clearInterval(addIndustryPoint);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const rivePlayer = useRive({
+    src: chickenTest,
+    autoplay: true,
+    stateMachines: "State Machine 1",
+    layout: new Layout({
+      fit: Fit.Contain,
+      alignment: Alignment.TopCenter,
+    }),
+  });
+
+  const riveIndustry = useRive({
+    src: chickenTest,
+    autoplay: true,
+    stateMachines: "State Machine 1",
+    layout: new Layout({
+      fit: Fit.Contain,
+      alignment: Alignment.TopCenter,
+    }),
+  });
+
+  const explodePlayer = useStateMachineInput(
+    rivePlayer.rive,
+    "State Machine 1",
+    "explode"
+  );
+
+  const explodeIndustry = useStateMachineInput(
+    riveIndustry.rive,
+    "State Machine 1",
+    "explode"
+  );
+
+  const animOut = () => {
+    gsap.to(industryRef.current, {
+      filter: `blur(${blurAmount}px)`,
+      x: -500,
+      opacity: 0,
+      duration: 0.5,
+    });
+  };
+
+  let blurAmount = 5;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ userSelect: "none" }}>
+        <div>
+          <h1>Animal Killer</h1>
+          <p>Can you kill animals at the same rate as the meat industry?</p>
+        </div>
+        <div className="GameContainer">
+          <div className="IndustryContainer" style={{ overflow: "overflow" }}>
+            <h2>Industry</h2>
+            <div
+              onClick={() => {
+                explodeIndustry?.fire();
+                animOut();
+              }}
+              ref={industryRef}
+              style={{
+                filter: `blur(0px)`,
+                transform: "scale(5, 0.001)",
+              }}
+            >
+              <div
+                style={{
+                  height: "300px",
+                  width: "300px",
+                  transform: "scale(calc(1/5), 1000)",
+                }}
+              >
+                <riveIndustry.RiveComponent />
+              </div>
+            </div>
+            <h3>{`x ${industryCount}`}</h3>
+          </div>
+          <div className="PlayerContainer">
+            <h2>You</h2>
+            <div
+              style={{ height: "300px", width: "300px", cursor: "pointer" }}
+              onClick={handleIncreasePlayerCount}
+            >
+              <rivePlayer.RiveComponent />
+            </div>
+            <h3>{`x ${playerCount}`}</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/types/types.d.ts
@@ -0,0 +1,3 @@
+declare module '@assets/chicken-test.riv' {
+    export const chickenTest: string;
+};
